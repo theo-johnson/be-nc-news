93c9e1649278dd4019253a3b6f34828b83d7eaee
Carces <theo_johnson@ntlworld.com>
2023-02-21 16:09:10 +0000

    Removed a rogue console.log

--- controllers/endpoints-controllers.js
+++ controllers/endpoints-controllers.js
@@ -2,6 +2,5 @@ const { fetchEndpoints } = require('../models/endpoints-models');
 
 exports.getEndpoints = (req, res, next) => {
   const endpoints = fetchEndpoints();
-  console.log(endpoints, ' <<<<<<<<<<<<<<< ');
   res.status(200).send({ endpoints });
 };
