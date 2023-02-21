d6acf8a3a7a9102f4e86639ea18ac77340d56a34
Carces <theo_johnson@ntlworld.com>
2023-02-21 16:21:14 +0000

    End of tutorial step 5 commit

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,15 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
+const dbURL = process.env.DATABASE_URL;
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !dbURL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+const config = ENV === 'production' ? { connectionString: dbURL, max: 2 } : {};
+
+module.exports = new Pool(config);
