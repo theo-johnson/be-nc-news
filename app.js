01b0ba08b2609e0d2342e05fe56de302d374d0ec
Carces <theo_johnson@ntlworld.com>
2023-03-13 11:47:31 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const {
   handlePSQL400Errors,
   handlePSQL404Errors,
@@ -10,6 +11,7 @@ const {
 const apiRouter = require('./routes/api-router');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
